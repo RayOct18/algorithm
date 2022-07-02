92a7a0e807c09d392f8a0d8ec7d530847226083b
ray <birken1018@gmail.com>
2022-07-02 19:04:33 +0800

    improve quick sort

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,27 +1,28 @@
 
 
-def partition(l, h, nums):
-    pivot = nums[l]
-    i, j = l+1, h-1
+def partition(nums, l, r):
+    pivot = nums[r]
+    i, j = l, r-1
     while (i < j):
-        while nums[i] <= pivot:
-            i += 1
-        while nums[j] > pivot:
-            j -= 1
-        if (i < j):
+        if nums[j] < pivot:
             nums[i], nums[j] = nums[j], nums[i]
-    nums[l], nums[j] = nums[j], nums[l]
+            i += 1
+        else:
+            j -= 1
+    if nums[j] < pivot:
+        j += 1
+    nums[r], nums[j] = nums[j], nums[r]
     return j
 
-def quick_sort(nums, l, h):
-    if (l < h):
-        j = partition(l, h, nums)
-        quick_sort(nums, l, j)
-        quick_sort(nums, j+1, h-1)
+def quick_sort(nums, l, r):
+    if (l < r):
+        j = partition(nums, l, r)
+        quick_sort(nums, l, j-1)
+        quick_sort(nums, j+1, r)
 
 
 if __name__ == '__main__':
     nums = [8, 1, 3, 2, 4, 6, 7, 9, 11, 16, 5]
-    nums.append(float('inf'))
-    quick_sort(nums, 0, len(nums))
-    print(nums[:-1])
+    quick_sort(nums, 0, len(nums)-1)
+    print(nums)
+
